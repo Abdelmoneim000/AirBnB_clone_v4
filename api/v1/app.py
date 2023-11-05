7476e221e50276be71b67d1529c96890f949db57
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-11-05 16:57:27 +0200

    finish task 3, changing routes and test the response of a GET method and changing the circles

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-""" Flask Application """
+"""The Developnment of a REST API"""
+
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
+from flask import Flask, jsonify
+import os
 from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
 
+# enable CORS and allow for origins:
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def close_app(exc):
+    """when the app is close"""
+    storage.close()
+
+# Error handlers fr expected app behavior:
+@app.errorhandler(404)
+def error_handler(e):
+    """Return 404"""
+    data = {"error": "Not found"}
+    return jsonify(data), 404
+
+
+if __name__ == "__main__":
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = os.environ.get('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
+
 
 
 @app.teardown_appcontext
